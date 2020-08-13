1fba9efac7a0ffb401b5d2790e1dc39ea316f5ce
alireza <alirezashateri7@gmail.com>
2020-08-13 23:32:06 +0430

    Add single perceptron algorithm

---
+++ ch02/perceptron.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+
+class Perceptron:
+    """Perceptron classifier.
+
+    Parameters
+    ------------
+    eta : float
+        Learning rate (between 0.0 and 1.0)
+    epochs : int
+        Passes over the training dataset
+    random_state : int
+        Random number generator seed for random weight
+        initialization.
+
+    Attributes
+    ------------
+    w_ : 1d-array
+        Weights after fitting.
+    errors_ : list
+        Number of miss_classifications (updates) in each epoch.
+
+    """
+
+    def __init__(self, eta=0.01, epochs=50, random_state=1):
+        self.eta = eta
+        self.epochs = epochs
+        self.random_state = random_state
+        self.w_ = None
+        self.errors_ = None
+
+    def fit(self, X: np.ndarray, Y: np.ndarray):
+        """Fit training data.
+
+        Parameters
+        ------------
+        X : {array-like}, shape = [n_examples, n_features]
+            Training vectors, where
+
+        :param X:
+        :param Y:
+        :return:
+        """
+        rgen = np.random.RandomState(seed=self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + Y.size)
+
+        self.errors_ = []
+
+        for _ in range(self.epochs):
+            error = 0
+            for x, y in zip(X, Y):
+                predicted_y = self.predict(x)
+                update = (y - predicted_y) * self.eta
+                self.w_[1:] += update * x
+                self.w_[0] = self.w_[0] + update
+
+                error += int(update != 0.0)
+            self.errors_.append(error)
+        return self
+
+    def net_input(self, x: np.ndarray):
+        return x.dot(self.w_[1:]) + self.w_[0]
+
+    def predict(self, x: np.ndarray):
+        return np.where(self.net_input(x) > 0, 1, -1)
