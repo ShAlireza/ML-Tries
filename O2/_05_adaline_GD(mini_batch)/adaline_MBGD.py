6ccd2c4bd8a4e1b874cbb431ef924ec1f6971f9b
alireza <alirezashateri7@gmail.com>
2020-08-15 12:13:08 +0430

    Add adaline using mini batch method in Gradient descent

--- O2/_05_adaline_GD(mini_batch)/adaline_MBGD.py
+++ O2/_05_adaline_GD(mini_batch)/adaline_MBGD.py
@@ -1,9 +1,9 @@
-from typing import Tuple, Union
+from typing import Tuple
 
 import numpy as np
 
 
-class AdalineSGD:
+class AdalineMBGD:
     """ADAptive LInear NEuron classifier.
 
     Parameters
@@ -17,9 +17,8 @@ class AdalineSGD:
         initialization.
     activation : function
         Optional activation function provided by user
-    shuffle : bool
-        Shuffles training data every epoch if True to prevent
-        cycles.
+    batch_size : int
+        Count of training data used in every epoch
 
     Attributes
     ----------
@@ -31,18 +30,17 @@ class AdalineSGD:
     """
 
     def __init__(self, eta=0.01, epochs=50, random_state=1, activation=None,
-                 shuffle=True):
+                 batch_size=50):
         self.eta = eta
         self.epochs = epochs
         self.random_state = random_state
-        self.shuffle = shuffle
+        self.batch_size = batch_size
         self._activation = activation
         self.w_ = None
-        self.w_initialized = False
-        self.random_generator = np.random.RandomState(seed=self.random_state)
+        self.random_generator = np.random.RandomState(seed=random_state)
         self.cost_ = None
 
-    def fit(self, X: np.ndarray, Y: np.ndarray):
+    def fit(self, X: np.ndarray, Y: np.ndarray) -> 'AdalineMBGD':
         """Fit training data.
 
         Parameters
@@ -56,56 +54,37 @@ class AdalineSGD:
 
         Returns
         -------
-        self : AdalineSGD
+        self : AdalineGD
 
         """
-        self._initialize_weights(X.shape[0])
+        random_generator = np.random.RandomState(seed=self.random_state)
+        self.w_ = random_generator.normal(loc=0.0, scale=0.01,
+                                          size=1 + X.shape[1])
 
         self.cost_ = []
 
         for _ in range(self.epochs):
-            if self.shuffle:
-                X, y = self._shuffle(X, Y)
-            cost = 0
-            for x, y in zip(X, Y):
-                cost += self._update_weights(x, y)
-            avg_cost = cost / Y.size
-            self.cost_.append(avg_cost)
-
+            X_m, Y_m = self._mini_batch(X, Y)
+            net_input = self.net_input(X_m)
+            output = self.activation(net_input)
+            errors = Y_m - output
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors ** 2).sum() / 2
+            self.cost_.append(cost)
         return self
 
-    def partial_fit(self, X: np.ndarray, Y: np.ndarray) -> 'AdalineSGD':
-        """Fit training data without reinitializing the weights"""
-        if not self.w_initialized:
-            self._initialize_weights(X.shape[1])
-        if Y.ravel().shape[0] > 1:
-            for x, y in zip(X, Y):
-                self._update_weights(x, y)
-        else:
-            self._update_weights(X, Y)
-        return self
-
-    def _shuffle(self, X: np.ndarray,
-                 Y: np.ndarray) -> 'Tuple[np.ndarray, np.ndarray]':
-        """Shuffle training data"""
-        indexes = self.random_generator.permutation(Y.size)
-        return X[indexes], Y[indexes]
+    def _mini_batch(self, X: np.ndarray,
+                    Y: np.ndarray) -> 'Tuple[np.ndarray, np.ndarray]':
+        """Get a random mini batch from complete data"""
+        random_indexes = self.random_generator.choice(np.arange(0, Y.size),
+                                                      size=self.batch_size)
+        return X[random_indexes], Y[random_indexes]
 
     def _initialize_weights(self, size: int) -> 'None':
         """Initialize weights to small random numbers"""
         self.w_ = self.random_generator.normal(loc=0.0, scale=0.01,
                                                size=1 + size)
-        self.w_initialized = True
-
-    def _update_weights(self, x: np.ndarray,
-                        target: Union[float, np.ndarray]) -> 'float':
-        """Apply Adaline learning rule to update weights"""
-        output = self.activation(self.net_input(x))
-        error = target - output
-        self.w_[1:] = self.eta * x.dot(error)
-        self.w_[0] = self.eta * error
-        cost = error ** 2 / 2.0
-        return cost
 
     def activation(self, X: np.ndarray) -> 'np.ndarray':
         """Apply linear activation function"""
@@ -115,6 +94,6 @@ class AdalineSGD:
         """Calculate net input"""
         return X.dot(self.w_[1:]) + self.w_[0]
 
-    def predict(self, X: np.ndarray) -> np.ndarray:
+    def predict(self, X: np.ndarray) -> 'np.ndarray':
         """Return class label prediction"""
         return np.where(self.net_input(X) > 0, 1, -1)
