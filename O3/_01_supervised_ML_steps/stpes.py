2b43984df8296abd9b3bc591a3113e810cde2f08
alireza <alirezashateri7@gmail.com>
2020-08-15 15:39:04 +0430

    Add The five main steps in training supervised machine learning algorithms

---
+++ O3/_01_supervised_ML_steps/stpes.py
@@ -0,0 +1,16 @@
+"""
+    The five main steps that are involved in training a supervised
+    machine learning algorithm can be summarized as follows:
+"""
+
+steps = """    
+    1. Selecting features and collecting labeled training examples.
+    
+    2. Choosing a performance metric.
+    
+    3. Choosing a classifier and optimization algorithm.
+    
+    4. Evaluating the performance of the model.
+    
+    5. Tuning the algorithm.
+"""
