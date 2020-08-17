3a4c882198a6288e2e6bed1525787c9b20f4e1ea
alireza <alirezashateri7@gmail.com>
2020-08-17 12:38:25 +0430

    Add sigmoid function and visualize it

---
+++ O3/_08_logistic_regression_scikitlearn/sigmoid_function.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+# visualize sigmoid function
+
+def sigmoid_visual():
+    z = np.arange(-7, 7, 0.1)
+    phi_z = sigmoid(z)
+    plt.plot(z, phi_z)
+    plt.axvline(0.0, color='k')
+    plt.ylim(-0.1, 1.1)
+    plt.xlabel('z')
+    plt.ylabel('$\phi (z)$')
+
+    plt.yticks([0.0, 0.5, 1.0])
+    ax = plt.gca()
+    ax.yaxis.grid(True)
+    plt.tight_layout()
+    plt.show()
