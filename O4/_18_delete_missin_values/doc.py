fb065c85953f7990ff8a20b0d72f814c09adbadc
alireza <alirezashateri7@gmail.com>
2020-08-22 13:16:14 +0430

    Add delete_missing_values doc

---
+++ O4/_18_delete_missin_values/doc.py
@@ -0,0 +1,11 @@
+"""
+
+    Although the removal of m`issing data seems to be a convenient approach, it
+    also comes with certain disadvantages; for example, we may end up removing
+    too many samples, which will make a reliable analysis impossible. Or, if we
+    remove too many feature columns, we will run the risk of losing valuable
+    information that our classifier needs to discriminate between classes. In
+    the next example, we will look at one of the most commonly used
+    alternatives for dealing with missing values: interpolation techniques.`
+
+"""
