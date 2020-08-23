268af3acc2e3ece1ebd6b5efd505821adbac0034
alireza <alirezashateri7@gmail.com>
2020-08-23 19:59:41 +0430

    Add train_test_separation

--- O4/_21_train_test_separation/train_test_separation.py
+++ O4/_21_train_test_separation/train_test_separation.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 from O4 import prepare_data
 
