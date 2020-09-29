4516be7a3275c9bdedd7bd258800be384b6b34f0
alireza <alirezashateri7@gmail.com>
2020-09-29 16:06:12 +0330

    Add kernel pca

--- O5/__init__.py
+++ O5/__init__.py
@@ -1,3 +1,4 @@
 from .utils import prepare_data_wine
+from ._26_kernel_principal_component_analysis.kpca import rbf_kernel_pca
 
-__all__ = ('prepare_data_wine',)
+__all__ = ('prepare_data_wine', 'rbf_kernel_pca')
